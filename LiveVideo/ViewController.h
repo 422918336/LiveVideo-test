37797fdc9e6e662239aab9723faf31ae29737e07
QianjinLiu <QJ@cpe-66-61-30-212.neo.res.rr.com>
2016-07-02 19:07:23 +0800

    Initial Commit

---
+++ LiveVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveVideo
+//
+//  Created by QJ on 16/7/2.
+//  Copyright © 2016年 QJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LiveVideo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiveVideo
+//
+//  Created by QJ on 16/7/2.
+//  Copyright © 2016年 QJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
